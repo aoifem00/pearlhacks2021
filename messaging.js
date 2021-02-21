6c2c59114fd83bc00af4ea8363d379c27fce414a
axy-olotl <41653005+axy-olotl@users.noreply.github.com>
2021-02-21 00:39:59 -0500

    Create messaging.js

---
+++ messaging.js
@@ -0,0 +1,7 @@
+// based on http://www.materialup.com/posts/dailyui-13-direct-messaging
+// will add emojis soon https://codepen.io/genarocolusso/pen/QEajPy
+
+$(".head").on("click", function(){
+   $(".friends_list").toggleClass("active");
+  
+})
